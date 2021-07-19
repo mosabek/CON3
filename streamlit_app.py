0afb0828a99b4e025f7c4de167cf0f1446ffe8cd
mosabek <80618355+mosabek@users.noreply.github.com>
2021-07-19 21:29:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,8 +67,8 @@ img_str = img_str.decode('ascii')
 
 ## Construct the URL to retrieve image.
 upload_url = ''.join([
-    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-    '?access_token=vbIBKNgIXqAQ',
+    'https://detect.roboflow.com/?model=excavotor-3&version=8',
+    '?api_key=jp6GFrGWHhf0W01LEwkU',
     '&format=image',
     f'&overlap={overlap_threshold * 100}',
     f'&confidence={confidence_threshold * 100}',
@@ -95,8 +95,8 @@ st.image(image,
 
 ## Construct the URL to retrieve JSON.
 upload_url = ''.join([
-    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-    '?access_token=vbIBKNgIXqAQ'
+    'https://detect.roboflow.com/?model=excavotor-3&version=8',
+    '?api_key=jp6GFrGWHhf0W01LEwkU',
 ])
 
 ## POST to the API.
