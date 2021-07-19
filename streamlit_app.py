e96ac55b8b89b6d39f43b626f61e87b7b220c1bc
mosabek <80618355+mosabek@users.noreply.github.com>
2021-07-19 22:01:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,6 @@ output_dict = r.json()
 
 ## Summary statistics section in main app.
 st.write('### Summary Statistics')
-st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
 
 ## Histogram in main app.
 st.write('### Histogram of Confidence Levels')
